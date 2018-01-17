30b2c7a2e5a4169ff708a27274a19fc45aa95621
jurintal1 <jussi.rintala@gmail.com>
2018-01-17 21:15:49 +0200

    tehtävä 1.1

---
+++ osa1/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Hello = (props) => {
+    return (
+      <div>
+        <p>Hello {props.name}</p>
+      </div>
+    )
+  }
+
+  const App = () => {
+    return (
+      <div>
+        <h1>Greetings</h1>
+        <Hello name="Arto" />
+        <Hello name="Pekka" />
+      </div>
+    )
+  }
+  
+  ReactDOM.render(<App />, document.getElementById('root'))
