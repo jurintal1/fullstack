9a5b52fded9121b4c615495930b067a97dfd0b11
jurintal1 <jussi.rintala@gmail.com>
2018-01-17 21:34:42 +0200

    tehtävä 1.2

--- tehtava1_1/src/index.js
+++ tehtava1_1/src/index.js
@@ -30,18 +30,27 @@ const Otsikko = (props) => {
 
 const Sisalto = (props) => {
     return (
+            <div>
+                <Osa osa={props.osa1} tehtavia={props.tehtavia1} />
+                <Osa osa={props.osa2} tehtavia={props.tehtavia2} />
+                <Osa osa={props.osa3} tehtavia={props.tehtavia3} />    
+    </div>
+    )
+}
+
+const Osa = (props) => {
+    return (
         <div>
-            <p> {props.osa1} {props.tehtavia1} </p>
-            <p> {props.osa2} {props.tehtavia2} </p>
-            <p> {props.osa3} {props.tehtavia3} </p>
+           <p> {props.osa} {props.tehtavia} </p> 
         </div>
+
     )
 }
 
 const Yhteensa = (props) => {
     return (
         <div>
-            <p>Yhteensä {props.tehtavia1 + props.tehtavia2 + props.tehtavia3} tehtävää </p>
+            <p> yhteensä {props.tehtavia1 + props.tehtavia2 + props.tehtavia3} tehtävää </p>
         </div>
     )
 
