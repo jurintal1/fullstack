6cb5f1ab2ba3503de618650e85dd92b275bfc803
jurintal1 <jussi.rintala@gmail.com>
2018-01-21 23:11:48 +0200

    tehtävä 1.8

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+
+const Statistics = ({hyva, neutraali, huono}) => (
+    <div>
+        <p>keskiarvo {(hyva - huono) / (huono + neutraali + hyva)} </p>
+        <p>positiivisia {hyva * 100 / (hyva + huono + neutraali)}%</p>
+    </div>
+)
 
 
 class App extends React.Component {
@@ -13,33 +25,43 @@ class App extends React.Component {
         }
     }
 
+    lisaaHyva = () => {
+        this.setState({hyva : this.state.hyva + 1})
+    }
+
+    lisaaNeutraali = () => {
+        this.setState({neutraali : this.state.neutraali + 1})
+    }
+
+    lisaaHuono = () => {
+        this.setState({huono : this.state.huono + 1})
+    }
+   
+
     render() {
         return (
             <div>
             <h1>anna palautetta</h1>
-            <button onClick = {
-                        () => this.setState({hyva : this.state.hyva + 1}) }>
-                hyvä
-            </button>
-            <button onClick = {() => this.setState({neutraali : this.state.neutraali + 1}) }>
-                neutraali
-            </button><button onClick = {() => this.setState({huono : this.state.huono + 1}) }>
-                huono
-            </button>
+            <Button text='hyvä' handleClick={this.lisaaHyva} />
+            <Button text='neutraali' handleClick={this.lisaaNeutraali} />
+            <Button text='huono' handleClick={this.lisaaHuono} />
+            
 
             <h1>statistiikka</h1>
             
             <p>hyvä {this.state.hyva}</p>
             <p>neutraali {this.state.neutraali}</p>
             <p>huono {this.state.huono}</p>
+
+            <Statistics hyva={this.state.hyva} neutraali={this.state.neutraali} huono={this.state.huono} />
             
-            <p>keskiarvo {(this.state.hyva - this.state.huono) / 
-                (this.state.huono + this.state.neutraali + this.state.hyva)} </p>
-            <p>positiivisia {this.state.hyva * 100 / (this.state.hyva + this.state.huono + this.state.neutraali)}%</p>
+            
 
             </div>
         )
     }
+
+    
 }
 
 
