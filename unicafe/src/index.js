0b71d8756b65f74da41150107c114ee57f3c3a33
jurintal1 <jussi.rintala@gmail.com>
2018-01-22 23:01:26 +0200

    tehtävä 1.11

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -3,23 +3,40 @@ import ReactDOM from 'react-dom';
 
 const Button = ({ handleClick, text }) => (
     <button onClick={handleClick}>
-      {text}
+        {text}
     </button>
-  )
+)
 
-const Statistics = ({hyva, neutraali, huono}) => {
+const Statistics = ({ hyva, neutraali, huono }) => {
     if (hyva + neutraali + huono === 0) {
         return (
             <p>Palautetta ei ole vielä tullut.</p>
         )
     }
     return (
-        <div>
-            <p>keskiarvo {(hyva - huono) / (huono + neutraali + hyva)} </p>
-            <p>positiivisia {hyva * 100 / (hyva + huono + neutraali)}%</p>
-        </div>
+        <table>
+            <tbody>
+                <tr>
+                    <td>
+                        keskiarvo
+                    </td>
+                    <td>
+                        {((hyva - huono) / (huono + neutraali + hyva)).toFixed(2)}
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        positiivisia
+                    </td>
+                    <td>
+                        {(hyva * 100 / (hyva + huono + neutraali)).toFixed(1)}%
+                    </td>
+                </tr>
+            </tbody>
+
+        </table>
     )
-    
+
 }
 
 
@@ -29,55 +46,72 @@ class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            hyva : 0,
-            neutraali : 0,
-            huono : 0
+            hyva: 0,
+            neutraali: 0,
+            huono: 0
         }
     }
 
     lisaaHyva = () => {
-        this.setState({hyva : this.state.hyva + 1})
+        this.setState({ hyva: this.state.hyva + 1 })
     }
 
     lisaaNeutraali = () => {
-        this.setState({neutraali : this.state.neutraali + 1})
+        this.setState({ neutraali: this.state.neutraali + 1 })
     }
 
     lisaaHuono = () => {
-        this.setState({huono : this.state.huono + 1})
+        this.setState({ huono: this.state.huono + 1 })
     }
-   
+
     plus = (palaute) => {
-        
+
         console.log(palaute)
         return () => {
-          this.setState({[palaute] : this.state[palaute] + 1 }) 
+            this.setState({ [palaute]: this.state[palaute] + 1 })
         }
-      }
+    }
 
     render() {
         return (
             <div>
-            <h1>anna palautetta</h1>
-            <Button text='hyvä' handleClick={this.plus('hyva')} />
-            <Button text='neutraali' handleClick={this.plus('neutraali')} />
-            <Button text='huono' handleClick={this.plus('huono')} />              
+                <h1>anna palautetta</h1>
+                <Button text='hyvä' handleClick={this.plus('hyva')} />
+                <Button text='neutraali' handleClick={this.plus('neutraali')} />
+                <Button text='huono' handleClick={this.plus('huono')} />
 
-            <p>hyvä {this.state.hyva}</p>
-            <p>neutraali {this.state.neutraali}</p>
-            <p>huono {this.state.huono}</p>
+                <br />
+                <br />
 
-            <h1>statistiikka</h1>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>hyvä</td>
+                            <td>{this.state.hyva}</td>
+                        </tr>
+                        <tr>
+                            <td>neutraali</td>
+                            <td>{this.state.neutraali}</td>
+                        </tr>
+                        <tr>
+                            <td>huono</td>
+                            <td>{this.state.huono}</td>
+                        </tr>
+                    </tbody>
 
-            <Statistics hyva={this.state.hyva} neutraali={this.state.neutraali} huono={this.state.huono} />
-            
-            
+                </table>
+
+                <h1>statistiikka</h1>
+
+                <Statistics hyva={this.state.hyva} neutraali={this.state.neutraali} huono={this.state.huono} />
+
+
 
             </div>
         )
     }
 
-    
+
 }
 
 
@@ -86,7 +120,7 @@ class App extends React.Component {
 
 ReactDOM.render(
     <App />,
-     document.getElementById('root')
+    document.getElementById('root')
 )
 
 
